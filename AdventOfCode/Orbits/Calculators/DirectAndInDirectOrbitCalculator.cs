16492671c9b749ae3942cfdee9a9291943aae61e
Adam Brewer <adam.brewer@mab.org.uk>
2020-08-21 16:06:27 +0100

    DAY 6 part 2 wip

--- AdventOfCode/Orbits/Calculators/DirectAndInDirectOrbitCalculator.cs
+++ AdventOfCode/Orbits/Calculators/DirectAndInDirectOrbitCalculator.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Orbits.Interfaces;
 using AdventOfCode.Orbits.Models;
 
@@ -14,8 +16,58 @@ namespace AdventOfCode.Orbits.Calculators
         
         public int Calculate(IRelationshipInputData data)
         {
-            var (_, totalOrbits) = _orbitMapper.Create(new RelationshipData(data));
-            return totalOrbits;
+            var relationshipData = new RelationshipData(data);
+
+            var parents = relationshipData.Data.ToDictionary(k => k.Value, v => v.Key);
+            var objs = relationshipData.Data.SelectMany(s => new List<string> {s.Key, s.Value}).Distinct().ToList();
+
+            var orbs = new Dictionary<string, int>();
+
+            int GetOrbs(string p)
+            {
+                if (orbs.ContainsKey(p)) return orbs[p];
+                
+                if (parents.ContainsKey(p))
+                {
+                    orbs[p] = 1 + GetOrbs(parents[p]);
+                }
+                else
+                {
+                    orbs[p] = 0;
+                }
+
+                return orbs[p];
+            }
+
+            var sum = objs.Select(GetOrbs).Sum();
+            
+            var youPath = new List<string> {"YOU"};
+            while (parents.ContainsKey(youPath.Last()))
+            {
+                youPath.Add(parents[youPath.Last()]);
+            }
+
+            var sanPath = new List<string> {"SAN"};
+            while (parents.ContainsKey(sanPath.Last()))
+            {
+                sanPath.Add(parents[sanPath.Last()]);
+            }
+
+            var orbitsToSanta = 0;
+            for (var i = 0; i < youPath.Count; i++)
+            {
+                for (var j = 0; j < sanPath.Count; j++)
+                {
+                    var p = youPath[i];
+
+                    if (sanPath.Contains(p))
+                    {
+                        orbitsToSanta = i + j - 2;
+                    }
+                }
+            }
+
+            return sum;
         }
     }
 }
