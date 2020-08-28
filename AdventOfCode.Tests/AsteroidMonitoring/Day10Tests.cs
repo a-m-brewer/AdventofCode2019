242a97ffd739d6a111fa2034eeac19f287406fd9
Adam Brewer <adam.brewer@mab.org.uk>
2020-08-28 15:41:53 +0100

    Day 10 pt 1

--- AdventOfCode.Tests/AsteroidMonitoring/Day10Tests.cs
+++ AdventOfCode.Tests/AsteroidMonitoring/Day10Tests.cs
@@ -33,10 +33,11 @@ namespace AdventOfCode.Tests.AsteroidMonitoring
                 "......#.#.",
                 "#..#.#....",
                 "..#######.",
-                ".#.#.###..",                
-                "..#....#.#",                
+                ".#.#.###..",
+                ".#..#.....",
+                "..#....#.#",
                 "#..#....#.",
-                ".##.#..###",                
+                ".##.#..###",
                 "##...#..#.",
                 ".#....####"
             });
@@ -45,6 +46,96 @@ namespace AdventOfCode.Tests.AsteroidMonitoring
             var (asteroid, visible) = map.FindBestLocation();
             asteroid.Position.Should().BeEquivalentTo(new Coordinates(5, 8));
             visible.Should().Be(33);
+        }
+
+        [Test]
+        public void Part1Example3()
+        {
+            var raw = new RawMap(new List<string>
+            {
+                "#.#...#.#.",
+                ".###....#.",
+                ".#....#...",
+                "##.#.#.#.#",
+                "....#.#.#.",
+                ".##..###.#",
+                "..#...##..",           
+                "..##....##",                
+                "......#...",                
+                ".####.###."
+            });
+            var map = new Map(raw);
+
+            var (asteroid, visible) = map.FindBestLocation();
+            asteroid.Position.Should().BeEquivalentTo(new Coordinates(1, 2));
+            visible.Should().Be(35);
+        }
+        
+        [Test]
+        public void Part1Example4()
+        {
+            var raw = new RawMap(new List<string>
+            {
+                ".#..#..###",
+                "####.###.#",
+                "....###.#.",
+                "..###.##.#",
+                "##.##.#.#.",
+                "....###..#",
+                "..#.#..#.#",
+                "#..#.#.###",
+                ".##...##.#",
+                ".....#.#.."
+            });
+            var map = new Map(raw);
+
+            var (asteroid, visible) = map.FindBestLocation();
+            asteroid.Position.Should().BeEquivalentTo(new Coordinates(6, 3));
+            visible.Should().Be(41);
+        }
+        
+        [Test]
+        public void Part1Example5()
+        {
+            var raw = new RawMap(new List<string>
+            {
+                ".#..##.###...#######",
+                "##.############..##.",
+                ".#.######.########.#",
+                ".###.#######.####.#.",
+                "#####.##.#.##.###.##",
+                "..#####..#.#########",
+                "####################",
+                "#.####....###.#.#.##",
+                "##.#################",
+                "#####.##.###..####..",
+                "..######..##.#######",
+                "####.##.####...##..#",
+                ".#####..#.######.###",
+                "##...#.##########...",
+                "#.##########.#######",
+                ".####.#.###.###.#.##",
+                "....##.##.###..#####",
+                ".#.#.###########.###",
+                "#.#.#.#####.####.###",
+                "###.##.####.##.#..##"
+            });
+            var map = new Map(raw);
+
+            var (asteroid, visible) = map.FindBestLocation();
+            asteroid.Position.Should().BeEquivalentTo(new Coordinates(11, 13));
+            visible.Should().Be(210);
+        }
+        
+        [Test]
+        public void Part1Exercise()
+        {
+            var raw = new FileRawMap("day10.txt");
+            var map = new Map(raw);
+
+            var (asteroid, visible) = map.FindBestLocation();
+            asteroid.Position.Should().BeEquivalentTo(new Coordinates(26, 28));
+            visible.Should().Be(267);
         }
     }
 }
